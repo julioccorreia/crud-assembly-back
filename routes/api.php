e8503c3e4715fb6d0b65d21b2c56075fb95c72ff
Julio Gazige <juliocgazige@hotmail.com>
2024-11-09 19:29:13 -0300

    atualizando rotas da api

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\JogosController;
+use App\Http\Controllers\UsuariosController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -16,4 +18,17 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::prefix('/usuarios')->group(function (){
+    Route::post('/cadastrar', [UsuariosController::class, 'cadastrar']);
+    Route::post('/login', [UsuariosController::class, 'login']);
+});
+
+
+Route::prefix('/jogos')->group(function (){
+    Route::post('/cadastrar', [JogosController::class, 'cadastrar']);
+    Route::get('/listar/{idUsuario}', [JogosController::class, 'listar']);
+    Route::put('/editar/{idJogo}', [JogosController::class, 'editar']);
+    Route::delete('/excluir/{idJogo}', [JogosController::class, 'excluir']);
 });
